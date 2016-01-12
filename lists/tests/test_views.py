f2dba07ca73cceaf3b5ba942a03b95e3878fae69
Peter Ho <peterho1024@gmail.com>
2016-01-12 21:25:19 +0800

    rename all item input ids and names. still broken

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -64,7 +64,7 @@ class ListViewTest(TestCase):
 
         self.client.post(
                 '/lists/%d/' % correct_list.id,
-                data={'item_text': 'A new item for an existing list'}
+                data={'text': 'A new item for an existing list'}
         )
 
         self.assertEqual(Item.objects.count(), 1)
@@ -78,7 +78,7 @@ class ListViewTest(TestCase):
 
         response = self.client.post(
                 '/lists/%d/' % correct_list.id,
-                data={'item_text': 'A new item for an existing list'}
+                data={'text': 'A new item for an existing list'}
         )
         self.assertRedirects(response, '/lists/%d/' % correct_list.id)
 
@@ -86,7 +86,7 @@ class ListViewTest(TestCase):
         list_ = List.objects.create()
         response = self.client.post(
                 '/lists/%d/' % list_.id,
-                data={'item_text': ''}
+                data={'text': ''}
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(Item.objects.count(), 0)
@@ -99,7 +99,7 @@ class NewListTest(TestCase):
     def test_saving_a_POST_request(self):
         self.client.post(
                 '/lists/new',
-                data={'item_text': 'A new list item'}
+                data={'text': 'A new list item'}
         )
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
@@ -108,7 +108,7 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post(
                 '/lists/new',
-                data={'item_text': 'A new list item'}
+                data={'text': 'A new list item'}
         )
         new_list = List.objects.first()
         self.assertRedirects(response, '/lists/%d/' % new_list.id)
